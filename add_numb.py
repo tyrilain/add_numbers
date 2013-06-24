b6a1a683a95fee9170d1b60549b80aa3dd1ffd44
Tara Martin <taramartin@gmail.com>
2013-06-24 13:56:47 -0400

    Replaced add_two with add_three

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,8 @@
-def add_two(a,b):
-	return a+b
+def add_three(a,b,c):
+	return a+b+c
 
 def main():
-	print ("2+4 =", add_two(2,4))
+	print ("2+3+4 =", add_three(2,3,4))
 
 if __name__ == "__main__":
 	main()
