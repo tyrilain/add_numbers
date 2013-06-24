811c6364423f5dfdad31dca11d82e06bf2789562
Tara Martin <taramartin@gmail.com>
2013-06-24 14:05:42 -0400

    Testing merge from master

--- add_numb.py
+++ add_numb.py
@@ -1,12 +1,8 @@
 def add_three(a,b,c):
 	return a+b+c
 
-def add_two(a,b):
-	return a+b
-
 def main():
 	print ("2+3+4 =", add_three(2,3,4))
-	print ("2+3 = ", add_two(2,3))
 
 if __name__ == "__main__":
 	main()
